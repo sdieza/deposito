25654d8c8608338cb24fe05e0888590c7acdc551
Sergio <peopleadsl@gmail.com>
2023-03-12 15:30:46 +0100

    Creación de comentarios

--- src/Cuentas/CCuenta.java
+++ src/Cuentas/CCuenta.java
@@ -1,113 +1,173 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @author Sergio Diez Alonso
+ * @version 1.0
  */
-
+/**
+ * Package Cuentas
+ */
 package Cuentas;
 
-
+/**
+ * Clase Cuentas, que contiene los métodos de la clase "Cuentas"
+ */
 public class CCuenta {
 
-
+    // Parametros de la clase "CCuenta" 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-
-    public CCuenta()
-    {
+    /**
+     * Método constructor vacio de la clase "CCuenta".
+     */
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    /**
+     * Método constructor de la clase "CCuenta".
+     *
+     * @param nom parametro String del Nombre.
+     * @param cue Parametro String del número de cuenta.
+     * @param sal Parametro Double del saldo de la cuenta.
+     * @param tipo parametro Double del tipo de interes.
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
-    public void asignarNombre(String nom)
-    {
+    /**
+     * Método asignarNombre que asigna un nombre al objeto clase.
+     *
+     * @param nom Parametro String del nombre que se asigna al objeto.
+     */
+    public void asignarNombre(String nom) {
         setNombre(nom);
     }
 
-    public String obtenerNombre()
-    {
+    /**
+     * Método ObtenerNombre que obtiene el nombre de un objeto.
+     *
+     * @return getNombre que devuelve el valor nombre del objeto.
+     */
+    public String obtenerNombre() {
         return getNombre();
     }
 
-
-    public double estado()
-    {
+    /**
+     * Método estado que obtiene el saldo del objeto.
+     *
+     * @return getSaldo que retorna un Double del valor del saldo del objeto.
+     */
+    public double estado() {
         return getSaldo();
     }
 
-
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     * Método Ingresar del Objeto "CCuenta"
+     *
+     * @param cantidad Parametro Double de la cantidad que queremos añadir al
+     * objeto "CCuenta"
+     * @throws Exception Controlamos la excepción de errores.
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         setSaldo(getSaldo() + cantidad);
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    /**
+     * Método retirar que permite eliminar una cantidad del objeto "CCuenta".
+     *
+     * @param cantidad Parametro Double que es la cantidad que se retirará del
+     * objeto "CCuenta".
+     * @throws Exception Controlamos las excepciones del método.
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         setSaldo(getSaldo() - cantidad);
     }
 
-
-    public String obtenerCuenta()
-    {
+    /**
+     * Método ObtenerCuenta que permite recuperar el valor de la cuenta.
+     *
+     * @return getCuenta devuelve un valor String con el valor de la cuenta.
+     */
+    public String obtenerCuenta() {
         return getCuenta();
     }
 
-
+    /**
+     * Método getNombre que permite recuperar el valor del nombre del objeto.
+     *
+     * @return devuelve un String con el valor del nombre del objeto.
+     */
     public String getNombre() {
         return nombre;
     }
 
-
+    /**
+     * Método setNombre que añade el String nombre el objeto "CCuenta"
+     *
+     * @param nombre Parametro String con el valor nombre para añadir al objeto
+     * "CCuenta"
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-
+    /**
+     * Método getTipoInteres que recupera el tipo de interes.
+     *
+     * @return tipoInteres un Double con el valor del tipo de interés.
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-
+    /**
+     * Método setTipoInteres que establece el valor de tipoInteres
+     * @param tipoInterés un double que estable el valor tipoInteres.
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
     /**
-     * @return the cuenta
+     * Método getCuenta que devuelve la cuenta
+     * @return cuenta, un String con el valor de cuenta del objeto.
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Método setCuenta que establece la cuenta
+     * @param cuenta, establece un String con el número de cuenta.
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Método getSaldo que devuelve el saldo de la cuenta.
+     * @return saldo, devuelve el saldo del objeto.
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Método setSaldo que establece el saldo del objeto cuenta.
+     * @param saldo, devuelve el saldo del objeto.
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
